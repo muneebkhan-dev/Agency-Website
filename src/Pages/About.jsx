bdfb6c9a29e4f5d6cb34b6d9014430fb9e3e93c9
Muneeb khan <muneebkan18069@gmail.com>
2026-06-12 09:00:43 -1200

    making about us page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from "react"
 
 const About = () => {
   return (
-    <h1>About US</h1>
+    <section className='bg-[#222831] min-h-screen px-10 mx-auto'>
+      <div className='max-w-7xl w-full'>
+        <div className="w-full flex ">
+          <div className="w-1/2">
+            <h3 className="text-base text-[#00D0B0] font-bold ">About us</h3>
+            <p className="font-playfair font-semibold text-white text-3xl">
+              We build websites based on client needs and of course the target
+              market, because that's our expertise
+            </p>
+            <div className="font-dm text-white">
+            <p>
+              We create delightful digital experiences that make life simpler
+              and more enjoyable. We create enterprise software that has the
+              polished, snappy feel of the best consumer apps.
+            </p>
+            <p>
+              From Slack to Facebook, we team up with marketing departments of
+              large companies and startups to deliver award‑winning websites.
+            </p>
+            </div>
+          </div>
+          <div className="w-1/2">
+            <img className="" src="/public/images/Group 48095475.png" alt="" />
+          </div>
+        </div>
+        <div className="">
+        <img src="/public/images/Group 14.png" alt="" />
+        </div>
+      </div>
+    </section>
   )
 }
 
