01adabceb42101e2346ee070621830961d125131
Muneeb khan <muneebkan18069@gmail.com>
2026-07-08 04:27:45 -1200

    hero section and services section was made responsive

--- src/Pages/Home/Components/Hero.jsx
+++ src/Pages/Home/Components/Hero.jsx
@@ -4,15 +4,15 @@ import { HiArrowLongRight } from "react-icons/hi2"
 const Hero = () => {
   return (
     <section className='relative flex bg-[#222831]'>
-      <div className='cont-1 relative min-h-110 md:min-h-110 lg:min-h-105 mx-auto'>
-        <div className='hero-sec max-w-7xl px-8 lg:px-15 mx-auto mt-5 md:mt-13 lg:mt-10 w-full flex items-center md:text-center lg:text-start flex-col lg:flex-row text-[#FFFFFF] md:gap-2 lg:gap-20'>
+      <div className='cont-1 relative min-h-117 md:min-h-110 lg:min-h-105 mx-auto'>
+        <div className='hero-sec max-w-7xl px-8 lg:px-15 mx-auto mt-10 md:mt-13 lg:mt-10 w-full flex items-center md:text-center lg:text-start flex-col lg:flex-row text-[#FFFFFF] md:gap-2 lg:gap-20'>
           <div className='w-full md:2/3 lg:w-1/2'>
-            <h1 className='heading text-4xl md:text-5xl lg:text-6xl leading-10 md:leading-15 lg:leading-17 font-playfair font-semibold '>
+            <h1 className='heading text-3xl md:text-5xl lg:text-6xl leading-8 md:leading-15 lg:leading-17 font-playfair font-semibold '>
               We are <span className='text-[#00D0B0]'>always</span> there for
               all your needs
             </h1>
           </div>
-          <div className=' md:w-2/3 lg:w-1/2 flex flex-col my-5 gap-8 md:gap-10'>
+          <div className=' md:w-2/3 lg:w-1/2 flex flex-col my-5 gap-6 md:gap-10'>
             <div className='para-1 font-dm font-medium text-sm md:text-base lg:text-base leading-6 lg:leading-7 text-white/70'>
               We are a remote design agency based in Montreal, working with
               clients around the world. As passionate designers, we love
@@ -29,22 +29,23 @@ const Hero = () => {
         </div>
         <div>
           <img
-            className='absolute bottom-0 left-1/2 object-cover object-left md:object-center h-52 md:h-60 lg:h-70 w-full md:w-full lg:w-[80%] -translate-x-1/2 translate-y-1/2 md:translate-y-1/2 rounded-2xl lg:rounded-none'
+            className='meetImg absolute bottom-0 left-1/2 object-cover object-left md:object-center h-60 md:h-60 lg:h-70 w-full md:w-full lg:w-[80%] -translate-x-1/2 translate-y-1/2 md:translate-y-1/2 rounded-2xl lg:rounded-none'
             src='/public/images/unsplash_hCb3lIB8L8E.png'
             alt=''
           />
         </div>
+        <img
+        src='/public/images/Rectangle 381.png'
+        alt='decorator img'
+        className='decorator-img md:hidden lg:hidden absolute w-1/2 -top-15 -right-20'
+      />
       </div>
       <img
         className='hidden lg:flex absolute bottom-0 left-0 w-1/4 translate-y-1/2'
         src='/public/images/Rectangle 380.png'
         alt=''
       />
-      <img
-        src='/public/images/Rectangle 381.png'
-        alt='decorator img'
-        className='decorator-img md:hidden lg:hidden absolute w-1/2 -top-20 -right-30'
-      />
+      
     </section>
   )
 }
--- src/Pages/Home/Components/ServicesSection.jsx
+++ src/Pages/Home/Components/ServicesSection.jsx
@@ -29,13 +29,13 @@ const ServicesSection = () => {
   ]
 
   return (
-    <section className='relative min-h-screen flex items-center'>
-      <div className='max-w-7xl px-28 mx-auto flex flex-col gap-10'>
-        <div className="pt-30">
-          <h1 className="font-DMmono text-[#00D0B0] font-semibold py-4">SERVICES</h1>
-          <div className="w-full flex gap-20">
-            <h1 className="font-playfair w-1/2 text-[#0F0B33] text-4xl font-semibold">Where our services add value to your product</h1>
-            <p className="font-dm font-semibold text-sm w-1/2 leading-6 text-[#222831]/60">
+    <section className='relative min-h-screen flex items-start'>
+      <div className='max-w-7xl py-15 px-5 md:px-10 lg:px-28 mx-auto flex flex-col gap-10'>
+        <div className="div-2 pt-30 flex flex-col items-center md:items-start lg:items-start text-center md:text-start lg:text-start">
+          <h1 className="font-DMmono text-[#00D0B0] w-3/4 text-sm lg:text-base font-semibold py-4 md:py-2 lg:py-4">SERVICES</h1>
+          <div className="div-3 w-full flex flex-col items-center md:flex-row lg:flex-row md:justify-between gap-4 md:gap-4 lg:gap-20">
+            <h1 className="serviceHead font-playfair w-full md:w-1/2 lg:w-1/2 text-[#0F0B33] text-2xl md:text-3xl lg:text-4xl font-semibold">Where our services add value to your product</h1>
+            <p className="font-dm font-medium text-base md:text-sm lg:text-base w-ful md:w-1/2 lg:w-1/2 leading-6 text-[#222831]/60">
               Helping companies & individuals identify key solutions for their
               target markets. We boost their ability to create products. Our
               business model saves clients time and money. Don't reinvent the
@@ -43,12 +43,12 @@ const ServicesSection = () => {
             </p>
           </div>
         </div>
-          <div className="flex justify-between py-10 w-full">
+          <div className="cards flex flex-col items-center md:items-start lg:items-start md:flex-row lg:flex-row justify-between gap-20 py-10 w-full">
             {service.map((service) => (
-              <div key={service.id} className="card flex flex-col items-center gap-3 w-1/4">
-                <img src={service.image} alt="image" className="1/2"/>
-                <h1 className="font-dm font-bold text-[#0F0B33] text-xl">{service.title}</h1>
-                <p className="font-dm text-base text-center text-[#5a5a61] ">{service.description}</p>
+              <div key={service.id} className="card flex flex-col items-center p-8 md:p-0 lg:p-0 gap-3 w-full md:w-1/4 lg:w-1/4 shadow-xl md:shadow-none lg:shadow-none rounded-2xl md:rounded-none lg:rounded-none">
+                <img src={service.image} alt="image" className="w-1/3"/>
+                <h3 className="font-dm font-bold text-[#0F0B33] text-[20px] lg:text-xl">{service.title}</h3>
+                <p className="font-dm text-base md:text-sm lg:text-base text-center text-[#5a5a61] ">{service.description}</p>
               </div>
             ))}
           
