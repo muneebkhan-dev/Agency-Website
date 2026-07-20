2ec3af0564156a3c074dc0f199d02fc2f242dc1e
Muneeb khan <muneebkhan18069@gmail.com>
2026-07-20 06:36:26 -1200

    feat: add form state and partial event handlers

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { MdEmail } from "react-icons/md"
 import { RxPerson } from "react-icons/rx"
 import { MdMailOutline } from "react-icons/md"
@@ -16,6 +16,25 @@ import { FaFacebook } from "react-icons/fa"
 import { FaTelegramPlane } from "react-icons/fa"
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  })
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log(formData)
+  }
+
   return (
     <section className='relative min-h-screen'>
       <div className=''>
@@ -23,7 +42,9 @@ const Contact = () => {
           <h2 className='font-DMmono text-sm md:text-base lg:text-base font-bold text-[#00D0B0] tracking-[2px]'>
             / CONTACT
           </h2>
-          <h3 className='font-dm font-bold text-3xl md:text-5xl lg:text-5xl'>Get in touch with us</h3>
+          <h3 className='font-dm font-bold text-3xl md:text-5xl lg:text-5xl'>
+            Get in touch with us
+          </h3>
           <div className='font-dm text-sm md:text-base lg:text-base text-center text-[#222831]/70 flex flex-col items-center gap-2'>
             <p>Have a project in mind or want to work together?</p>
             <p>
@@ -34,14 +55,17 @@ const Contact = () => {
         </div>
         <div className='contact-content max-w-7xl px-5 md:px-10 lg:px-30 py-10 md:py-20 lg:py-20 mx-auto flex flex-col md:flex-row lg:flex-row gap-10 md:gap-7 lg:gap-15'>
           <div className='w-auto md:w-3/5 lg:w-3/5 shadow-xl rounded-2xl p-8 border-2 border-slate-100'>
-            <form className='flex text-start flex-col gap-3'>
+            <form onSubmit={handleSubmit} className='flex text-start flex-col gap-3'>
               <div>
                 <h3 className='text-base font-bold py-2'>Full Name</h3>
                 <div className='relative border-2 border-slate-100 rounded-xl'>
                   <input
+                    name='name'
+                    value={formData.name}
+                    onChange={handleChange}
                     type='text'
                     placeholder='Enter your name'
-                    className='py-2 px-4 border-hidden font-dm placeholder:font-medium placeholder:text-base placeholder:text-gray-400 outline-none'
+                    className='w-full pr-10 md:pr-0 lg:pr-0 py-2 px-4 border-hidden font-dm placeholder:font-medium text-sm md:text-base lg:text-base placeholder:text-gray-400 outline-none'
                   />
                   <RxPerson className='absolute size-5 right-4 top-1/2 -translate-y-1/2' />
                 </div>
@@ -50,9 +74,12 @@ const Contact = () => {
                 <h3 className='text-base font-bold py-2'>Email Address</h3>
                 <div className='relative border-2 border-slate-100 rounded-xl'>
                   <input
+                    name='email'
+                    value={formData.email}
+                    onChange={handleChange}
                     type='email'
                     placeholder='Enter your email'
-                    className='py-2 px-4 font-dm placeholder:font-medium placeholder:text-base placeholder:text-gray-400 outline-none'
+                    className='w-full pr-10 md:pr-0 lg:pr-0 py-2 px-4 font-dm placeholder:font-medium text-sm md:text-base lg:text-base placeholder:text-gray-400 outline-none'
                   />
                   <MdMailOutline className='absolute size-5 right-4 top-1/2 -translate-y-1/2' />
                 </div>
@@ -61,32 +88,39 @@ const Contact = () => {
                 <h3 className='text-base font-bold py-2'>Subject</h3>
                 <div className='relative border-2 border-slate-100 rounded-xl'>
                   <input
-                    type='text'
+                    name='subject'
+                    value={formData.subject}
+                    onChange={handleChange}
+                    type='subject'
                     placeholder='Enter your subject'
-                    className='py-2 px-4 font-dm placeholder:font-medium placeholder:text-base placeholder:text-gray-400 outline-none'
+                    className='w-full pr-10 md:pr-0 lg:pr-0 py-2 px-4 font-dm placeholder:font-medium text-sm md:text-base lg:text-base placeholder:text-gray-400 outline-none'
                   />
                   <IoDocumentTextOutline className='absolute size-5 right-4 top-1/2 -translate-y-1/2 text-gray-600' />
                 </div>
               </div>
               <div>
                 <h3 className='text-base font-bold py-2'>Message</h3>
-                <div className='relative border-2 border-slate-100 rounded-xl min-h-40 py-4 px-4'>
-                  <input
+                <div className='relative border-2 border-slate-100 rounded-xl  py-4 px-4'>
+                  <textarea
+                    name='message'
+                    value={formData.message}
+                    onChange={handleChange}
+                    type='message'
                     placeholder='Write your message here...'
-                    className='font-dm placeholder:font-medium placeholder:text-base placeholder:text-gray-400 flex justify-start outline-none'
-                  ></input>
+                    className='w-full min-h-40 font-dm placeholder:font-medium text-sm md:text-base lg:text-base placeholder:text-gray-400 flex justify-start outline-none'
+                  ></textarea>
                   <MdOutlineEdit className='absolute size-5 right-5 top-3/4 -translate-y-1/2 text-gray-600' />
                 </div>
               </div>
               <div className='flex items-start'>
-                <button className='font-dm px-5 py-3 rounded-xl text-sm font-bold bg-[#00D0B0] text-white flex gap-2 items-center'>
+                <button onSubmit={handleSubmit} className='font-dm px-5 py-3 rounded-xl text-sm font-bold bg-[#00D0B0] text-white flex gap-2 items-center'>
                   <PiTelegramLogoBold className='text-white size-4' />
                   Send Message
                 </button>
               </div>
             </form>
           </div>
-          <div className="py-10">
+          <div className='py-10'>
             <div className='flex items-start gap-4 py-5  w-auto lg:w-4/5'>
               <div className='bg-[#F0FDFA] rounded-full p-2'>
                 <IoPersonOutline className='text-[#0D9488] size-6' />
@@ -107,7 +141,9 @@ const Contact = () => {
                 </div>
                 <div className='flex flex-col gap-2'>
                   <h4 className='font-dm text-base font-bold'>Email</h4>
-                  <p className='font-dm text-base text-gray-500'>hi@myport.com</p>
+                  <p className='font-dm text-base text-gray-500'>
+                    hi@myport.com
+                  </p>
                 </div>
               </div>
               <div className='flex items-start gap-4 py-5 w-auto lg:w-4/5 border-b border-gray-300'>
